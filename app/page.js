85c8836a44ccfecb7a27aa9d51cd69bfdf79575a
gowreesh <gowreesh369@gmail.com>
2024-08-18 03:19:04 +0530

    final2

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import supabase from "@/config/Supabase_Client";
 import useUser from "@/hooks/useUser";
 import { useEffect } from "react";
 
 export default function Home() {
+  const handleSignIn = () => {
+    window.location.href = "https://analytics3.vercel.app/signin";
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button onClick={handleSignIn}>Sign in</Button>
+    </main>
   );
 }
