8358ecfb9c4aa6b982d9033e90c13a112ef38848
gowreesh <gowreesh369@gmail.com>
2024-08-18 03:19:01 +0530

    env2

--- config/Supabase_Client.js
+++ config/Supabase_Client.js
@@ -1,8 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://xguwkgecsaiifephwigv.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhndXdrZ2Vjc2FpaWZlcGh3aWd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMTE1MjUsImV4cCI6MjA0NTc4NzUyNX0.JD6cygSHeAr9LlpaoL3uTc-I4uFOAYjKQBWynj-wFsc";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
 
 export default supabase;
